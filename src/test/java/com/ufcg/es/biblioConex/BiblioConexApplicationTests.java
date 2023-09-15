875cac155cf6bdda85be77457ce454aff0e93545
Lucas Leones <96065550+lleones@users.noreply.github.com>
2023-09-15 07:58:37 -0300

    Spring Initializr

---
+++ src/test/java/com/ufcg/es/biblioConex/BiblioConexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufcg.es.biblioConex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioConexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
